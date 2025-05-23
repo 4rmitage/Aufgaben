149def0d687606d1365510ce20536e3cef5bf5fa
Ivan <bla@blo.bl>
2025-05-23 11:22:36 +0200

    v1.2

--- Print.py
+++ Print.py
@@ -1,1 +1,27 @@
 print("Hello World")
+print(1+2)
+print(1-2)
+print(2*2)
+print(2/2)
+print(1.2+3.4)
+print(3.7/0.3)
+print('Hello World "Hello Animals" Hello People')
+age = 41
+
+print(age)
+
+age = 42
+
+print(age)
+
+salami = 1+10
+
+print(salami)
+
+print("string" + "verkettung")
+
+print(10 + 3)
+
+# code ausführen nicht möglich
+
+print(10 + "string")
