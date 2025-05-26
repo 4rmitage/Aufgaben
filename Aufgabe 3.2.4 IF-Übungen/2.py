5d99bdd6510a431b83d6667daeb8c6418747bf8a
Ivan <bla@blo.bl>
2025-05-26 20:07:58 +0200

    v1

---
+++ Aufgabe 3.2.4 IF-Übungen/2.py
@@ -0,0 +1,7 @@
+zahl = float(input("Bitte eine Zahl eingeben: "))
+if zahl > 0:
+    print("Die Zahl ist positiv.")
+elif zahl < 0:
+    print("Die Zahl ist negativ.")
+else:
+    print("Die Zahl ist null.")
