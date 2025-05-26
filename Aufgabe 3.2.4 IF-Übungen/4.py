a6dab237110b4be1e1e5452798ac8bd390c0c3a0
Ivan <bla@blo.bl>
2025-05-26 20:09:43 +0200

    v1

---
+++ Aufgabe 3.2.4 IF-Übungen/4.py
@@ -0,0 +1,15 @@
+note = int(input("Bitte eine Note zwischen 1 und 6 eingeben: "))
+if note == 1:
+    print("Sehr gut")
+elif note == 2:
+    print("Gut")
+elif note == 3:
+    print("Befriedigend")
+elif note == 4:
+    print("Ausreichend")
+elif note == 5:
+    print("Mangelhaft")
+elif note == 6:
+    print("ungenügend")
+else:
+    print("Ungültige Eingabe. Bitte eine Zahl zwischen 1 und 6.")
