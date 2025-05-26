e44d19ee7011e492ce2eea99f3527af34ebfdd8d
Ivan <bla@blo.bl>
2025-05-26 14:50:28 +0200

    v2

--- Aufgabe 3.2.3 IF.py
+++ Aufgabe 3.2.3 IF.py
@@ -2,3 +2,10 @@ age = int(input("Bitte gebe dein Alter ein:"))
 
 if age < 18:
     print("Achtung, der Nutzer ist jünger als 18")
+elif age == 18:
+    print("Der Nutzer ist exakt 18")
+elif age == 19:
+    print("Der Nutzer ist exakt 19")
+else:
+    print("Der Nutzer ist volljährig")
+print("Programmende")
