74b92057bfb87587e1525a1abdc9fa18f7363838
Ivan <bla@blo.bl>
2025-05-26 11:23:41 +0200

    v1

---
+++ Aufgabe 3.2.2 Operatoren.py
@@ -0,0 +1,10 @@
+value1 = int(input("Erste Zahl eingeben: "))
+value2 = int(input("Zweite Zahl eingeben: "))
+
+value1 += 7
+
+print(int(value1) + int(value2))
+
+
+# print(5 >= 5) - größer als, oder gleich
+# print(4 == 5) - (==) Gleich Operator, (!=) Ungleich Operator
