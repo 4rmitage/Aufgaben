f04ce8e69a21da986813b790420c60f1b5a9a34d
Ivan <bla@blo.bl>
2025-05-27 13:38:17 +0200

    v1

---
+++ Aufgabe 3.2.8 mit Listen arbeiten/1 Einfach.py
@@ -0,0 +1,12 @@
+# Liste von Zahlen
+zahlen = [4, 7, 2, 9, 1, 5, 3]
+
+# Länge der Liste ausgeben
+print("Die Länge der Liste ist:", len(zahlen))
+
+# Größte und kleinste Zahl ausgeben
+groesste_zahl = max(zahlen)
+kleinste_zahl = min(zahlen)
+
+print("Die größte Zahl ist:", groesste_zahl)
+print("Die kleinste Zahl ist:", kleinste_zahl)
